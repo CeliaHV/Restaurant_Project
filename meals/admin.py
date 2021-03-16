c68160942bcfb572b8e1a50a67d3836f539c250b
Celia Huamani <49768720+CeliaHV@users.noreply.github.com>
2021-03-16 12:10:43 -0300

    Add files via upload

---
+++ meals/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+# Register your models here.
+
+from .models import Meals , Category
+
+
+class MealsAdmin(SummernoteModelAdmin , admin.ModelAdmin):  # instead of ModelAdmin
+    summernote_fields = '__all__'
+    list_display = ['name', 'category', 'price', 'preparation_time']
+    search_fields = ['name', 'description']
+    list_filter = ['category', 'price']
+
+
+admin.site.register(Meals , MealsAdmin)
+admin.site.register(Category)
